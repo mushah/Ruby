26e47dfd63762192c352b40eb780a4f570717b13
Muhammad Shah <mushah@my.lonestar.edu>
2014-12-20 17:05:28 -0600

    12/20/2014 Learning Ruby the Hard Way

---
+++ ex3.rb
@@ -0,0 +1,28 @@
+puts 'I will now count my chickens:'
+
+#Adds 25 plus 30 hens and divides by 6
+puts "Hens #{25 + 30 / 6}"
+
+#subtracts the remainder of 25 times 3 divided by 4 from 100
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+puts 'Now I will count the eggs:'
+
+#Does a bunch of arithmetic
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+puts 'Is it true that 3 + 2 < 5 - 7?'
+
+#checks if 3+2 is less than 5-7 (False!)
+puts 3 + 2 < 5 - 7
+
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+
+puts "Oh, that's why it's false."
+
+puts 'How about some more.'
+
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
